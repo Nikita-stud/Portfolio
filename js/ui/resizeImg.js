9401fde0211aabce8b30bded668f3574e9b5b665
Nikita-stud <nikita151998de@gmail.com>
2024-09-29 22:45:40 +0200

    changes to img

--- js/ui/resizeImg.js
+++ js/ui/resizeImg.js
@@ -2,24 +2,33 @@ export function resizeImg(){
   const mainImgContainer = document.getElementById("main-img_container");
   const secondaryImgContainer = document.getElementById("inner-frame_img");
 
-  // if(window.innerWidth >= 1397){
-  //   secondaryImgContainer
-  // }
-  if(window.innerWidth){
-    let adjustPosition = window.innerWidth / 170;
+
+
+  if(window.innerWidth < 1200){
+    let adjustPosition = window.innerWidth / 140;
     let adjustWidth =window.innerWidth / 2.8;
     let adjustHeight =window.innerWidth / 4;
+
     secondaryImgContainer.style.width = `${adjustWidth}px`;
     secondaryImgContainer.style.height = `${adjustHeight}px`;
     secondaryImgContainer.style.top = `${adjustPosition}%`;
-    secondaryImgContainer.style.left = "18%";
+    secondaryImgContainer.style.left = "16%";
       
 
     console.log("Window: ", window.innerWidth)
     console.log("Adjusted: ", adjustPosition)
     console.log("Width: ", adjustWidth)
     console.log("Height: ", adjustHeight)
-
+  }
+  if(window.innerWidth >= 1200){
+      let adjustPosition = window.innerWidth / 170;
+      let adjustWidth =window.innerWidth / 2.8;
+      let adjustHeight =window.innerWidth / 4;
+  
+      secondaryImgContainer.style.width = `${adjustWidth}px`;
+      secondaryImgContainer.style.height = `${adjustHeight}px`;
+      secondaryImgContainer.style.top = `${adjustPosition}%`;
+      secondaryImgContainer.style.left = "18%";
   }
 
 };
