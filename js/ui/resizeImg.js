a5d9b9574ead5fe94458c53f8be66c7e1ab4fa11
Nikita-stud <nikita151998de@gmail.com>
2024-10-06 13:28:48 +0200

    Img in frame and js changes for easier frame controll

--- js/ui/resizeImg.js
+++ js/ui/resizeImg.js
@@ -1,36 +1,17 @@
 export function resizeImg(){
-  const mainImgContainer = document.getElementById("main-img_container");
-  const secondaryImgContainer = document.getElementById("inner-frame_img");
-  const carouselHeader = document.getElementById("carousel_header");
-
+  const secondaryImgContainer = document.getElementById("frame_img");
+  let adjustPosition = window.innerWidth / 220;
 
 
   if(window.innerWidth < 1200){
-    let adjustPosition = window.innerWidth / 140;
-    let adjustWidth =window.innerWidth / 2.8;
-    let adjustHeight =window.innerWidth / 4;
 
-    secondaryImgContainer.style.width = `${adjustWidth}px`;
-    secondaryImgContainer.style.height = `${adjustHeight}px`;
     secondaryImgContainer.style.top = `${adjustPosition}%`;
     secondaryImgContainer.style.left = "16%";
-      
-
-    console.log("Window: ", window.innerWidth)
-    console.log("Adjusted: ", adjustPosition)
-    console.log("Width: ", adjustWidth)
-    console.log("Height: ", adjustHeight)
   }
   if(window.innerWidth >= 1200){
-      let adjustPosition = window.innerWidth / 170;
-      let adjustWidth =window.innerWidth / 2.8;
-      let adjustHeight =window.innerWidth / 4;
 
-      carouselHeader.style.top= `${window.innerWidth / 85 +10}%`;
-      secondaryImgContainer.style.width = `${adjustWidth}px`;
-      secondaryImgContainer.style.height = `${adjustHeight}px`;
       secondaryImgContainer.style.top = `${adjustPosition}%`;
-      secondaryImgContainer.style.left = "18%";
+      secondaryImgContainer.style.left = "17%";
   }
 
 };
