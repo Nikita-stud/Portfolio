c0bbf38375f86ef9db0bd4a97a580cd60acc13ed
Nikita-stud <nikita151998de@gmail.com>
2024-10-08 15:24:54 +0200

    more text

--- js/ui/transformMainImg.js
+++ js/ui/transformMainImg.js
@@ -1,16 +1,30 @@
 const frame = document.getElementById("frame_img");
 const img = document.getElementById("inner-frame_img");
-const header = document.getElementById("main-header_container");
+const header = document.getElementById("main_header");
+const leftText = document.getElementById("left-main-img_text");
+const rightText = document.getElementById("right-main-img_text");
 
 export function transformMainImg(){
   img.addEventListener("click", ()=>{
 
     function toggleFrame(){
       frame.classList.toggle("frame_img-active")
+
+      function toggleLeftText(){
+        leftText.classList.toggle("main-img_text-active");
+      }
+      setTimeout(toggleLeftText, 1000);
+
+      function toggleRightText(){
+        rightText.classList.toggle("main-img_text-active");
+      }
+      setTimeout(toggleRightText, 3500);
     }
     setTimeout(toggleFrame, 500);
 
+
+
     img.classList.toggle("inner-frame_img-active");
-    header.classList.toggle("main-header_container-active");
+    header.classList.toggle("main-header-active");
   })
 }
