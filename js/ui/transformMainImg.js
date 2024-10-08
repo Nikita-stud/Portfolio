dfbef22d894ecd70f06cbb9bddbe55f705eb2467
Nikita-stud <nikita151998de@gmail.com>
2024-10-08 16:08:07 +0200

    almost done with upper part

--- js/ui/transformMainImg.js
+++ js/ui/transformMainImg.js
@@ -1,6 +1,7 @@
 const frame = document.getElementById("frame_img");
 const img = document.getElementById("inner-frame_img");
 const header = document.getElementById("main_header");
+const hiddenContainer = document.getElementById("hidden-text_container");
 const leftText = document.getElementById("left-main-img_text");
 const rightText = document.getElementById("right-main-img_text");
 
@@ -9,22 +10,25 @@ export function transformMainImg(){
 
     function toggleFrame(){
       frame.classList.toggle("frame_img-active")
+      header.classList.toggle("main-header-active");
+
+      hiddenContainer.classList.toggle("main-img_text-active")
 
       function toggleLeftText(){
         leftText.classList.toggle("main-img_text-active");
+        
+        function toggleRightText(){
+          rightText.classList.toggle("main-img_text-active");
+        }
+        setTimeout(toggleRightText, 2500);
       }
-      setTimeout(toggleLeftText, 1000);
+      setTimeout(toggleLeftText, 1500);
 
-      function toggleRightText(){
-        rightText.classList.toggle("main-img_text-active");
-      }
-      setTimeout(toggleRightText, 3500);
     }
     setTimeout(toggleFrame, 500);
 
 
 
     img.classList.toggle("inner-frame_img-active");
-    header.classList.toggle("main-header-active");
   })
 }
